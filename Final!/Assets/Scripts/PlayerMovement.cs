9ab7eb81bf6653d517d70809a65e08e1d3798e34
aramelix <144147993+aramelix@users.noreply.github.com>
2023-11-28 12:11:51 -0500

    Initial creation

---
+++ Final!/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //variables 
+    float speed;     //controls speed of movement 
+    float move;            //controls direction of movement 
+    public float jump;    // 
+    bool isJumping;         //tracks if object
+
+    Rigidbody2D rb;      //stores the rigidbody in the variable "rb"
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+        jump = 300f;
+        rb = GetComponent<Rigidbody2D>();   //get the rigidbody of the object 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");     //when any of the horizontal keys are pressed 
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);     //change the value of the rb to move 
+
+        //single jump limit 
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            isJumping = true;
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            isJumping = false;
+        }
+    }
+}
