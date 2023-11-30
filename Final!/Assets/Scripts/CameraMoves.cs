e0515d1ad478ea82b03d7fb79a8620c70ce49efe
aramelix <144147993+aramelix@users.noreply.github.com>
2023-11-29 23:03:24 -0500

    edits to scene and camera script

---
+++ Final!/Assets/Scripts/CameraMoves.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoves : MonoBehaviour
+{
+    //variables 
+    public GameObject target; 
+
+    // Update is called once per frame
+    void Update() 
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, -10);       //
+
+    }
+}
